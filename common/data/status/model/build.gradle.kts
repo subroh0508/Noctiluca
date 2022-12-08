c57727bfcadec5fed143ea8cb1ea5b0bbb10c422
subroh0508 <subroh.0508@gmail.com>
2022-12-08 23:02:29 +0900

    [add]Add dependency of kotlinx.datetime

--- common/data/status/model/build.gradle.kts
+++ common/data/status/model/build.gradle.kts
@@ -3,3 +3,13 @@ import org.jetbrains.compose.compose
 plugins {
     id("common-model")
 }
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlinx.datetime)
+            }
+        }
+    }
+}

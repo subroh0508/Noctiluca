4ccdecf3fc28ca68de5d14541fc7373f69e22fee
subroh0508 <subroh.0508@gmail.com>
2022-12-08 23:04:46 +0900

    [add]Add Status model

---
+++ common/data/status/model/src/commonMain/kotlin/noctiluca/status/model/Status.kt
@@ -0,0 +1,22 @@
+package noctiluca.status.model
+
+import kotlinx.datetime.LocalDateTime
+
+data class Status(
+    val id: String,
+    val content: String,
+    val warningText: String?,
+    val createdAt: LocalDateTime,
+    val visibility: Visibility,
+    val repliesCount: Int,
+    val favouriteCount: Int,
+    val reblogCount: Int,
+    val favourited: Boolean,
+    val reblogged: Boolean,
+    val tooter: Tooter,
+    val rebloggedBy: Tooter?
+) {
+    enum class Visibility { PUBLIC, UNLISTED, PRIVATE, DIRECT }
+
+    val isWarningContent get() = warningText?.isNotBlank() == true
+}
---
+++ common/data/status/model/src/commonMain/kotlin/noctiluca/status/model/Tooter.kt
@@ -0,0 +1,10 @@
+package noctiluca.status.model
+
+data class Tooter(
+    val id: String,
+    val username: String,
+    val displayName: String,
+    val url: String,
+    val avatar: String,
+    val avatarStatic: String,
+)

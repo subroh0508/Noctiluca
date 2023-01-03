fa04dc06b1fc40f9f1f8c4ca9a9d39e55eb150d1
subroh0508 <subroh.0508@gmail.com>
2023-01-04 00:44:43 +0900

    [edit]Wrap AsyncImage by IconButton

--- features/page/timeline/src/commonMain/kotlin/noctiluca/features/timeline/organisms/topappbar/CurrentInstanceTopAppBar.kt
+++ features/page/timeline/src/commonMain/kotlin/noctiluca/features/timeline/organisms/topappbar/CurrentInstanceTopAppBar.kt
@@ -1,10 +1,12 @@
 package noctiluca.features.timeline.organisms.topappbar
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import noctiluca.components.atoms.appbar.NavigateIconSize
 import noctiluca.components.atoms.appbar.TopAppBar
 import noctiluca.components.atoms.image.AsyncImage
@@ -20,7 +22,10 @@ fun CurrentInstanceTopAppBar() {
     TopAppBar(
         account.hostname?.value ?: getString().timeline_page_title,
         navigationIcon = {
-            IconButton(onClick = {}) {
+            IconButton(
+                onClick = {},
+                modifier = Modifier.padding(start = 8.dp),
+            ) {
                 AsyncImage(
                     account.current?.avatar,
                     //fallback = imageResources(getDrawables().icon_mastodon),

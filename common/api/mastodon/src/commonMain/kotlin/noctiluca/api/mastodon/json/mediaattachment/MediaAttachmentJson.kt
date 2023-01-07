99e32c81c8cc57d166e50912b0a94e5c3444b790
subroh0508 <subroh.0508@gmail.com>
2023-01-08 01:18:53 +0900

    [add]Add StatusJson

---
+++ common/api/mastodon/src/commonMain/kotlin/noctiluca/api/mastodon/json/mediaattachment/MediaAttachmentJson.kt
@@ -0,0 +1,21 @@
+package noctiluca.api.mastodon.json.mediaattachment
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ *  ref. https://docs.joinmastodon.org/entities/MediaAttachment/
+ */
+@Serializable
+data class MediaAttachmentJson(
+    val id: String,
+    val type: String,
+    val url: String,
+    @SerialName("preview_url")
+    val previewUrl: String,
+    @SerialName("remote_url")
+    val remoteUrl: String?,
+    // val meta: Meta,
+    val description: String,
+    val blurhash: String,
+)

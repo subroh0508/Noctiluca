d473dbe66d3117b77bf88d35fd8f62ce5950a79e
subroh0508 <subroh.0508@gmail.com>
2023-02-04 21:40:27 +0900

    [fix]Delete unnecessary eachPlugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,29 +6,6 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         includeBuild("plugins")
     }
-
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "org.jetbrains.kotlin.multiplatform",
-                "org.jetbrains.kotlin.android" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-                "org.jetbrains.plugin.serialization" -> useModule("org.jetbrains.kotlin:kotlin-serialization:${requested.version}")
-                "com.android.application",
-                "com.android.library" -> useModule("com.android.tools.build:gradle:${requested.version}")
-                "org.jetbrains.compose" -> useModule("org.jetbrains.compose:compose-gradle-plugin:${requested.version}")
-                "com.github.gmazzo.buildconfig" -> useModule("com.github.gmazzo:gradle-buildconfig-plugin:${requested.version}")
-            }
-        }
-    }
-    /*
-    plugins {
-        kotlin("multiplatform") version "1.7.20"
-        kotlin("android") version "1.7.20"
-        id("com.android.application") version "7.3.0"
-        id("com.android.library") version "7.3.0"
-        id("org.jetbrains.compose") version "1.2.1"
-    }
-    */
 }
 
 dependencyResolutionManagement {

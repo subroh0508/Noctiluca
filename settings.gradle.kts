720c6ae86c265c67f523976c05df61dff07a5dd0
subroh0508 <subroh.0508@gmail.com>
2023-02-04 17:32:31 +0900

    [fix]add useModule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
             when (requested.id.id) {
                 "org.jetbrains.kotlin.multiplatform",
                 "org.jetbrains.kotlin.android" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-                "org.jetbrains.plugin.serialization" -> ("org.jetbrains.kotlin:kotlin-serialization:${requested.version}")
+                "org.jetbrains.plugin.serialization" -> useModule("org.jetbrains.kotlin:kotlin-serialization:${requested.version}")
                 "com.android.application",
                 "com.android.library" -> useModule("com.android.tools.build:gradle:${requested.version}")
                 "org.jetbrains.compose" -> useModule("org.jetbrains.compose:compose-gradle-plugin:${requested.version}")

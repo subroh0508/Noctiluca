256a5f739a991cf152b7b40be929d15f3ea50fdf
subroh0508 <subroh.0508@gmail.com>
2023-03-11 21:38:06 +0900

    [fix]Divide test script

--- plugins/src/main/kotlin/test/report.gradle.kts
+++ plugins/src/main/kotlin/test/report.gradle.kts
@@ -1,19 +1,19 @@
 package test
 
 tasks.register(TASK_TEST_DEBUG_UNIT_TEST_REPORT, TestReport::class) {
-    destinationDir = file("$buildDir/reports/allTests/$TASK_TEST_DEBUG_UNIT_TEST")
+    destinationDir = file("$buildDir/reports/$TASK_TEST_DEBUG_UNIT_TEST")
 
     subprojects.forEach {
         val test = it.tasks.findByName(TASK_TEST_DEBUG_UNIT_TEST) as? Test
-        if (test != null) reportOn(test)
+        if (test != null) reportOn(test.binaryResultsDirectory.asFile.get())
     }
 }
 
 tasks.register(TASK_TEST_DESKTOP_TEST_REPORT, TestReport::class) {
-    destinationDir = file("$buildDir/reports/allTests/$TASK_TEST_DESKTOP_TEST")
+    destinationDir = file("$buildDir/reports/$TASK_TEST_DESKTOP_TEST")
 
     subprojects.forEach {
         val test = it.tasks.findByName(TASK_TEST_DESKTOP_TEST) as? Test
-        if (test != null) reportOn(test)
+        if (test != null) reportOn(test.binaryResultsDirectory.asFile.get())
     }
 }

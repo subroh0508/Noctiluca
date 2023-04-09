8ca6811d7e1166bad5f1dbbf4c857d27828a803e
subroh0508 <subroh.0508@gmail.com>
2023-04-10 01:52:03 +0900

    [edit]Lint

--- common/data/accountdetail/model/src/commonMain/kotlin/noctiluca/accountdetail/model/Relationship.kt
+++ common/data/accountdetail/model/src/commonMain/kotlin/noctiluca/accountdetail/model/Relationship.kt
@@ -1,15 +1,15 @@
 package noctiluca.accountdetail.model
 
 enum class Relationship {
-        FOLLOWING,
-        SHOWING_REBLOGS,
-        NOTIFYING,
-        FOLLOWED_BY,
-        BLOCKING,
-        BLOCKED_BY,
-        MUTING,
-        MUTING_NOTIFICATIONS,
-        REQUESTED,
-        DOMAIN_BLOCKING,
-        ENDORSED,
+    FOLLOWING,
+    SHOWING_REBLOGS,
+    NOTIFYING,
+    FOLLOWED_BY,
+    BLOCKING,
+    BLOCKED_BY,
+    MUTING,
+    MUTING_NOTIFICATIONS,
+    REQUESTED,
+    DOMAIN_BLOCKING,
+    ENDORSED,
 }

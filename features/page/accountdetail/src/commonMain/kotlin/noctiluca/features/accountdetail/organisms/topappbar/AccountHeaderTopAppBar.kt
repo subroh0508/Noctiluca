47a3716c879653fb66d47d8f300d84b20921c17e
subroh0508 <subroh.0508@gmail.com>
2023-04-30 23:53:49 +0900

    [edit]Lint

--- features/page/accountdetail/src/commonMain/kotlin/noctiluca/features/accountdetail/organisms/topappbar/AccountHeaderTopAppBar.kt
+++ features/page/accountdetail/src/commonMain/kotlin/noctiluca/features/accountdetail/organisms/topappbar/AccountHeaderTopAppBar.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier

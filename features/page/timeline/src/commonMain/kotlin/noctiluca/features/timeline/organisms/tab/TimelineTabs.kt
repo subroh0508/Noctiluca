00ef98572467b4f2768a8d105b438b5444e8b65d
subroh0508 <subroh.0508@gmail.com>
2023-05-07 19:12:52 +0900

    [add]Add PrimaryTabs

--- features/page/timeline/src/commonMain/kotlin/noctiluca/features/timeline/organisms/tab/TimelineTabs.kt
+++ features/page/timeline/src/commonMain/kotlin/noctiluca/features/timeline/organisms/tab/TimelineTabs.kt
@@ -1,11 +1,9 @@
 package noctiluca.features.timeline.organisms.tab
 
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import noctiluca.features.components.atoms.tab.PrimaryTabs
 import noctiluca.features.timeline.LocalTimelineListState
 import noctiluca.features.timeline.getString
 import noctiluca.timeline.domain.model.Timeline
@@ -20,18 +18,13 @@ internal fun TimelineTabs(
         return
     }
 
-    TabRow(
+    PrimaryTabs(
+        timelineListState.value,
         selectedTabIndex = timelineListState.currentTabIndex,
+        onClick = { index, _ -> timelineListState.setForeground(index) },
+        transform = { (timeline) -> timeline.label() },
         modifier = modifier,
-    ) {
-        timelineListState.value.forEachIndexed { index, (timeline, _, _, _, foreground) ->
-            Tab(
-                selected = foreground,
-                text = { Text(timeline.label()) },
-                onClick = { timelineListState.setForeground(index) },
-            )
-        }
-    }
+    )
 }
 
 @Composable

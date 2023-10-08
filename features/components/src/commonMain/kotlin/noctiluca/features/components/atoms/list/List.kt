3f9542eeffc59c67de2b59fd8b6ba71e33fb4a6d
subroh0508 <subroh.0508@gmail.com>
2023-10-09 01:35:26 +0900

    Fix Lint

--- features/components/src/commonMain/kotlin/noctiluca/features/components/atoms/list/List.kt
+++ features/components/src/commonMain/kotlin/noctiluca/features/components/atoms/list/List.kt
@@ -2,7 +2,6 @@ package noctiluca.features.components.atoms.list
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable

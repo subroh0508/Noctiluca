bc5abe496e7bcf7465d6c0c628c80dfb203dd7b6
subroh0508 <subroh.0508@gmail.com>
2023-11-21 01:20:50 +0900

    Delete ImageLoader

--- features/shared/src/desktopMain/kotlin/noctiluca/features/shared/utils/DesktopImageLoader.kt
+++
@@ -1,6 +0,0 @@
-package noctiluca.features.shared.utils
-
-import androidx.compose.ui.graphics.toComposeImageBitmap
-import org.jetbrains.skia.Image
-
-internal actual fun ByteArray.toImageBitmap() = Image.makeFromEncoded(this).toComposeImageBitmap()

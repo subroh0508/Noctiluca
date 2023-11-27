67353d5ec6bc6fa08fb582c6c6bdbc92b084466e
subroh0508 <subroh.0508@gmail.com>
2023-11-27 22:50:07 +0900

    Set fallback image

--- features/shared/src/commonMain/kotlin/noctiluca/features/shared/atoms/image/AsyncImage.kt
+++ features/shared/src/commonMain/kotlin/noctiluca/features/shared/atoms/image/AsyncImage.kt
@@ -7,7 +7,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import com.seiko.imageloader.ui.AutoSizeImage
+import com.seiko.imageloader.model.ImageAction
+import com.seiko.imageloader.rememberImageSuccessPainter
+import com.seiko.imageloader.ui.AutoSizeBox
 import noctiluca.model.Uri
 
 @Composable
@@ -18,20 +20,54 @@ fun AsyncImage(
     modifier: Modifier = Modifier,
     contentDescription: String? = null,
     fallback: Painter? = null,
-) = when {
-    uri != null -> AutoSizeImage(
-        uri.value,
-        alignment = alignment,
-        contentScale = contentScale,
-        modifier = modifier,
-        contentDescription = contentDescription,
-    )
-    fallback != null -> Image(
+) {
+    if (uri != null) {
+        AutoSizeBox(
+            uri.value,
+            contentAlignment = alignment,
+        ) { action ->
+            when (action) {
+                is ImageAction.Success -> Image(
+                    rememberImageSuccessPainter(action),
+                    contentDescription = contentDescription,
+                    contentScale = contentScale,
+                    modifier = modifier,
+                )
+
+                is ImageAction.Failure -> FallbackImage(
+                    fallback,
+                    contentScale,
+                    contentDescription,
+                    modifier,
+                )
+
+                is ImageAction.Loading -> Spacer(modifier)
+            }
+        }
+        return
+    }
+
+    FallbackImage(
         fallback,
+        contentScale,
         contentDescription,
         modifier,
-        alignment = alignment,
+    )
+}
+
+@Composable
+private fun FallbackImage(
+    painter: Painter?,
+    contentScale: ContentScale,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+) = if (painter != null) {
+    Image(
+        painter,
+        contentDescription,
+        modifier,
         contentScale = contentScale,
     )
-    else -> Spacer(modifier)
+} else {
+    Spacer(modifier)
 }

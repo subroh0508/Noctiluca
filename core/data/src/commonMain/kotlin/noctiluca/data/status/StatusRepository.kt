879deaaafaa7ec30ed86669e3deac6d6f2e3a686
subroh0508 <subroh.0508@gmail.com>
2024-01-03 14:52:45 +0900

    Add fetchStatus / fetchContext / delete methods

--- core/data/src/commonMain/kotlin/noctiluca/data/status/StatusRepository.kt
+++ core/data/src/commonMain/kotlin/noctiluca/data/status/StatusRepository.kt
@@ -1,8 +1,13 @@
 package noctiluca.data.status
 
+import noctiluca.model.StatusId
 import noctiluca.model.status.Status
 
 interface StatusRepository {
+    suspend fun fetch(id: StatusId): Status
+    suspend fun fetchContext(id: StatusId): List<Status>
+    suspend fun delete(id: StatusId): Status
+
     suspend fun favourite(status: Status): Status
     suspend fun boost(status: Status): Status
     suspend fun bookmark(status: Status): Status
--- core/data/src/commonMain/kotlin/noctiluca/data/status/impl/StatusRepositoryImpl.kt
+++ core/data/src/commonMain/kotlin/noctiluca/data/status/impl/StatusRepositoryImpl.kt
@@ -3,6 +3,7 @@ package noctiluca.data.status.impl
 import noctiluca.data.status.StatusRepository
 import noctiluca.data.status.toEntity
 import noctiluca.datastore.AuthenticationTokenDataStore
+import noctiluca.model.StatusId
 import noctiluca.model.status.Status
 import noctiluca.network.mastodon.MastodonApiV1
 
@@ -10,6 +11,22 @@ internal class StatusRepositoryImpl(
     private val api: MastodonApiV1,
     private val authenticationTokenDataStore: AuthenticationTokenDataStore,
 ) : StatusRepository {
+    override suspend fun fetch(
+        id: StatusId,
+    ) = api.getStatus(id.value).toEntity(authenticationTokenDataStore.getCurrent()?.id)
+
+    override suspend fun fetchContext(id: StatusId): List<Status> {
+        val (ancestors, descendants) = api.getStatusesContext(id.value)
+
+        return (ancestors + listOf(api.getStatus(id.value)) + descendants).map {
+            it.toEntity(authenticationTokenDataStore.getCurrent()?.id)
+        }
+    }
+
+    override suspend fun delete(
+        id: StatusId,
+    ) = api.deleteStatus(id.value).toEntity(authenticationTokenDataStore.getCurrent()?.id)
+
     override suspend fun favourite(status: Status): Status {
         val json =
             if (status.favourited) {

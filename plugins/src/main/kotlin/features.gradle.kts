3615e879cdaa163f645ee5203e4ca0915ea0398c
subroh0508 <subroh.0508@gmail.com>
2024-01-22 01:11:51 +0900

    Add dependency of org.jetbrains.compose.components:components-resources

--- plugins/src/main/kotlin/features.gradle.kts
+++ plugins/src/main/kotlin/features.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
                 implementation(compose.foundation)
                 implementation(compose.material3)
                 implementation(compose.materialIconsExtended)
+                implementation(compose.components.resources)
                 implementation(compose.preview)
 
                 implementation(libs.coroutinesCore)

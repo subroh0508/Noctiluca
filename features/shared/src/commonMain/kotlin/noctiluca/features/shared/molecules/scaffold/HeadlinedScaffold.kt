a3817fb3158f37aaa4b08928192f4934b132db13
subroh0508 <subroh.0508@gmail.com>
2024-01-27 17:00:49 +0900

    Remove unnecessary composable

--- features/shared/src/commonMain/kotlin/noctiluca/features/shared/molecules/scaffold/HeadlinedScaffold.kt
+++ features/shared/src/commonMain/kotlin/noctiluca/features/shared/molecules/scaffold/HeadlinedScaffold.kt
@@ -4,38 +4,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.GraphicsLayerScope
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Job
-import noctiluca.features.shared.atoms.image.AsyncImage
 import noctiluca.features.shared.model.LoadState
 import noctiluca.features.shared.model.LoadStateComposable
 import noctiluca.features.shared.molecules.list.LazyColumn
-import noctiluca.model.Uri
 
-private val ExpandedTopAppBarHeight = 152.dp
 private val CollapsedTopAppBarHeight = 64.dp
-
-private val HeaderHeightOffset = -(ExpandedTopAppBarHeight - CollapsedTopAppBarHeight)
-
-private val AvatarFrameSize = 104.dp
-private val AvatarIconSize = 96.dp
 
 private val HeadlinedScaffoldHorizontalPadding = 16.dp
 
@@ -276,71 +262,9 @@ fun HeadlineText(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HeadlineHeader(
-    header: Uri?,
-    scrollBehavior: TopAppBarScrollBehavior,
-    contentScale: ContentScale = ContentScale.FillHeight
-) {
-    val modifier = Modifier.height(ExpandedTopAppBarHeight)
-        .fillMaxWidth()
-        .graphicsLayer {
-            translationY = calculateTranslationY(scrollBehavior)
-        }
-
-    if (header == null) {
-        Spacer(
-            modifier = modifier.background(MaterialTheme.colorScheme.surface),
-        )
-    } else {
-        AsyncImage(
-            header,
-            contentScale = contentScale,
-            modifier = modifier,
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun HeadlineAvatar(
-    avatar: Uri?,
-    scrollBehavior: TopAppBarScrollBehavior,
-) = Box(
-    modifier = Modifier.size(AvatarFrameSize)
-        .offset(
-            x = HeadlinedScaffoldHorizontalPadding,
-            y = ExpandedTopAppBarHeight - AvatarFrameSize / 2,
-        )
-        .graphicsLayer {
-            translationY = calculateTranslationY(scrollBehavior)
-            alpha = 1.0F - scrollBehavior.state.collapsedFraction
-        }
-        .background(
-            color = MaterialTheme.colorScheme.surface,
-            shape = RoundedCornerShape(8.dp),
-        ),
-    contentAlignment = Alignment.Center,
-) {
-    AsyncImage(
-        avatar,
-        modifier = Modifier.size(AvatarIconSize)
-            .clip(RoundedCornerShape(8.dp)),
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
 @Suppress("MagicNumber")
 private fun rememberScrolledContainerColorAlpha(
     scrollBehavior: TopAppBarScrollBehavior,
 ): State<Float> = remember {
     derivedStateOf { scrollBehavior.state.collapsedFraction * 0.75F }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-private fun GraphicsLayerScope.calculateTranslationY(
-    scrollBehavior: TopAppBarScrollBehavior,
-): Float {
-    val offset = HeaderHeightOffset.toPx()
-    return if (offset < scrollBehavior.state.heightOffset) scrollBehavior.state.heightOffset else offset
 }

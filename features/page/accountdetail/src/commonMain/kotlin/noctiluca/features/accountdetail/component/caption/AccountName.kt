e9da62baa1eb41dfaf193838530b18d4754bfed7
subroh0508 <subroh.0508@gmail.com>
2024-01-29 02:01:10 +0900

    Show Bot label

--- features/page/accountdetail/src/commonMain/kotlin/noctiluca/features/accountdetail/component/caption/AccountName.kt
+++ features/page/accountdetail/src/commonMain/kotlin/noctiluca/features/accountdetail/component/caption/AccountName.kt
@@ -1,11 +1,11 @@
 package noctiluca.features.accountdetail.component.caption
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.filled.SmartToy
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -20,10 +20,21 @@ internal fun AccountName(
     detail: AccountAttributes,
     modifier: Modifier = Modifier,
 ) = Column(modifier) {
-    Text(
-        detail.displayName,
-        style = MaterialTheme.typography.titleLarge,
-    )
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Text(
+            detail.displayName,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.weight(1F),
+        )
+
+        if (detail.bot) {
+            BotLabel(
+                modifier = Modifier.align(Alignment.Bottom),
+            )
+        }
+    }
 
     Row {
         val baselineModifier = remember { Modifier.alignByBaseline() }
@@ -36,15 +47,53 @@ internal fun AccountName(
         )
 
         if (detail.locked) {
-            Icon(
-                Icons.Default.Lock,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.outline,
-                modifier = Modifier.baseline(Alignment.Bottom, topOffset = 4)
-                    .then(baselineModifier)
-                    .padding(start = 4.dp)
-                    .size(MaterialTheme.typography.titleMedium.fontSize.toDp()),
+            LockIcon(
+                baselineModifier = baselineModifier,
             )
         }
     }
 }
+
+@Composable
+private fun BotLabel(
+    modifier: Modifier = Modifier,
+) = Row(
+    modifier = Modifier.border(
+        width = 1.dp,
+        color = MaterialTheme.colorScheme.outline,
+        shape = AssistChipDefaults.shape,
+    )
+        .padding(vertical = 4.dp, horizontal = 8.dp)
+        .then(modifier),
+) {
+    val baselineModifier = remember { Modifier.alignByBaseline() }
+
+    Icon(
+        Icons.Default.SmartToy,
+        contentDescription = "Bot",
+        modifier = Modifier.size(MaterialTheme.typography.titleSmall.fontSize.toDp())
+            .baseline(Alignment.Bottom, topOffset = 6)
+            .then(baselineModifier),
+    )
+
+    Spacer(modifier = Modifier.width(4.dp))
+
+    Text(
+        "Bot",
+        style = MaterialTheme.typography.titleSmall,
+        modifier = baselineModifier,
+    )
+}
+
+@Composable
+private fun LockIcon(
+    baselineModifier: Modifier,
+) = Icon(
+    Icons.Default.Lock,
+    contentDescription = null,
+    tint = MaterialTheme.colorScheme.outline,
+    modifier = Modifier.baseline(Alignment.Bottom, topOffset = 4)
+        .then(baselineModifier)
+        .padding(start = 4.dp)
+        .size(MaterialTheme.typography.titleMedium.fontSize.toDp()),
+)

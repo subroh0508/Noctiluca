60641ab5b65483d7df8fa4ce11751cecba6df142
subroh0508 <subroh.0508@gmail.com>
2024-03-10 01:29:18 +0900

    Add Divider Context

--- features/page/statusdetail/src/commonMain/kotlin/noctiluca/features/statusdetail/component/StatusDetail.kt
+++ features/page/statusdetail/src/commonMain/kotlin/noctiluca/features/statusdetail/component/StatusDetail.kt
@@ -1,6 +1,5 @@
 package noctiluca.features.statusdetail.component
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -12,17 +11,12 @@ import androidx.compose.ui.unit.sp
 import noctiluca.features.shared.atoms.text.HtmlText
 import noctiluca.features.shared.status.Action
 import noctiluca.features.shared.utils.border
-import noctiluca.features.statusdetail.component.item.*
 import noctiluca.features.statusdetail.component.item.Actions
-import noctiluca.features.statusdetail.component.item.ConversationAxis
 import noctiluca.features.statusdetail.component.item.Count
 import noctiluca.features.statusdetail.component.item.CreatedAtAndVia
 import noctiluca.features.statusdetail.component.item.Header
-import noctiluca.features.statusdetail.component.item.Position
 import noctiluca.model.AccountId
-import noctiluca.model.StatusId
 import noctiluca.model.status.Status
-import noctiluca.features.shared.status.Status as ComposableStatus
 
 @Composable
 internal fun StatusDetail(
@@ -69,26 +63,5 @@ internal fun StatusDetail(
         favourited = status.favourited,
         bookmarked = status.bookmarked,
         onClickAction = onClickAction,
-    )
-}
-
-@Composable
-internal fun StatusItem(
-    status: Status,
-    position: Position,
-    onClickStatus: (StatusId) -> Unit,
-    onClickAvatar: (AccountId) -> Unit,
-    onClickAction: (Action) -> Unit,
-) = Row(
-    modifier = Modifier.clickable { onClickStatus(status.id) }
-        .height(IntrinsicSize.Min)
-        .padding(start = 8.dp),
-) {
-    ConversationAxis(position)
-
-    ComposableStatus(
-        status,
-        onClickAvatar = { onClickAvatar(it) },
-        onClickAction = { onClickAction(it) },
     )
 }
--- features/page/statusdetail/src/commonMain/kotlin/noctiluca/features/statusdetail/templates/StatusDetailScaffold.kt
+++ features/page/statusdetail/src/commonMain/kotlin/noctiluca/features/statusdetail/templates/StatusDetailScaffold.kt
@@ -1,9 +1,6 @@
 package noctiluca.features.statusdetail.templates
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -12,19 +9,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.LocalNavigator
-import noctiluca.features.navigation.navigateToAccountDetail
-import noctiluca.features.navigation.navigateToStatusDetail
-import noctiluca.features.statusdetail.component.StatusDetail
-import noctiluca.features.statusdetail.component.StatusItem
-import noctiluca.features.statusdetail.component.item.Position
+import noctiluca.features.statusdetail.section.StatusContext
 import noctiluca.features.statusdetail.section.StatusDetailTopAppBar
 import noctiluca.features.statusdetail.viewmodel.StatusDetailViewModel
-import noctiluca.model.AccountId
-import noctiluca.model.StatusId
-import noctiluca.model.status.StatusList
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -57,61 +45,8 @@ internal fun StatusDetailScaffold(
                 uiModel.getValue(),
                 lazyListState,
                 paddingValues,
-                onClickStatus = { navigator?.navigateToStatusDetail(it) },
-                onClickAvatar = { navigator?.navigateToAccountDetail(it) },
-            )
-        }
-    }
-}
-
-@Composable
-private fun StatusContext(
-    primary: StatusId,
-    statuses: StatusList,
-    lazyListState: LazyListState,
-    paddingValues: PaddingValues,
-    onClickStatus: (StatusId) -> Unit,
-    onClickAvatar: (AccountId) -> Unit,
-) {
-    HorizontalDivider(
-        modifier = Modifier.offset(y = paddingValues.calculateTopPadding()),
-    )
-
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        state = lazyListState,
-        contentPadding = PaddingValues(
-            start = paddingValues.calculateStartPadding(LayoutDirection.Ltr),
-            top = paddingValues.calculateTopPadding(),
-            end = paddingValues.calculateEndPadding(LayoutDirection.Ltr),
-            bottom = 64.dp,
-        ),
-    ) {
-        itemsIndexed(
-            statuses,
-            key = { _, status -> status.id.value },
-        ) { index, status ->
-            if (status.id == primary) {
-                StatusDetail(
-                    status,
-                    onClickAvatar = onClickAvatar,
-                    onClickAction = { },
-                )
-                return@itemsIndexed
-            }
-
-            val position = when (index) {
-                0 -> Position.TOP
-                statuses.size - 1 -> Position.BOTTOM
-                else -> Position.MIDDLE
-            }
-
-            StatusItem(
-                status,
-                position,
-                onClickStatus = onClickStatus,
-                onClickAvatar = onClickAvatar,
-                onClickAction = { },
+                onClickStatus = { navigator?.push(it) },
+                onClickAvatar = { navigator?.push(it) },
             )
         }
     }

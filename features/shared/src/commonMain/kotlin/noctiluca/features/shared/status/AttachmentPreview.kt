2a90ddf5893efde7c91e2a4b0c9b0cc610054e2d
subroh0508 <subroh.0508@gmail.com>
2024-04-06 21:57:07 +0900

    Show visibility off icon

--- features/shared/src/commonMain/kotlin/noctiluca/features/shared/status/AttachmentPreview.kt
+++ features/shared/src/commonMain/kotlin/noctiluca/features/shared/status/AttachmentPreview.kt
@@ -1,16 +1,18 @@
 package noctiluca.features.shared.status
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.SuggestionChipDefaults
@@ -23,15 +25,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import noctiluca.features.shared.components.image.AsyncImage
 import noctiluca.features.shared.getCommonString
-import noctiluca.model.Uri
+import noctiluca.features.shared.status.attachment.COUNT_PER_GRID
+import noctiluca.features.shared.status.attachment.GridHeight
+import noctiluca.features.shared.status.attachment.ThumbnailGrid
 import noctiluca.model.status.Attachment
-
-private const val COUNT_PER_GRID = 4
-private val GridHeight = 160.dp
 
 @Composable
 fun AttachmentPreview(
@@ -44,28 +45,21 @@ fun AttachmentPreview(
 
     SensitiveContent(
         sensitive = sensitive,
+        height = attachments.gridHeight,
     ) {
         Column {
-            attachments.filter { it !is Attachment.Audio && it !is Attachment.Unknown }
-                .chunked(COUNT_PER_GRID)
-                .forEach { chunked ->
-                    val previewUrls = chunked.map { it.previewUrl }
-
-                    when (chunked.size) {
-                        1 -> ThumbnailOne(previewUrls)
-                        2 -> ThumbnailTwo(previewUrls)
-                        3 -> ThumbnailThree(previewUrls)
-                        4 -> ThumbnailFour(previewUrls)
-                    }
-                }
+            ThumbnailGrid(attachments)
             Spacer(Modifier.height(8.dp))
         }
     }
 }
 
+private val List<Attachment>.gridHeight get() = GridHeight * ((size / COUNT_PER_GRID) + 1)
+
 @Composable
 private fun SensitiveContent(
     sensitive: Boolean,
+    height: Dp,
     content: @Composable () -> Unit,
 ) {
     if (!sensitive) {
@@ -76,118 +70,68 @@ private fun SensitiveContent(
     var showContent by remember { mutableStateOf(false) }
 
     if (showContent) {
-        content()
+        OverlayHideIcon(
+            height = height,
+            onHideContent = { showContent = false },
+            content = content,
+        )
         return
     }
 
+    SensitiveCover(
+        height = height,
+        onShowContent = { showContent = true },
+    )
+}
+
+@Composable
+private fun OverlayHideIcon(
+    height: Dp,
+    onHideContent: () -> Unit,
+    content: @Composable () -> Unit,
+) = Box(
+    modifier = Modifier.fillMaxWidth()
+        .height(height),
+) {
+    content()
+
     Box(
-        modifier = Modifier.fillMaxWidth()
-            .height(GridHeight)
+        modifier = Modifier.clickable { onHideContent() }
+            .offset(x = 4.dp, y = 4.dp)
             .clip(RoundedCornerShape(8.dp))
-            .background(MaterialTheme.colorScheme.surfaceBright),
+            .background(Color.Black.copy(0.6F))
+            .padding(4.dp),
     ) {
-        SuggestionChip(
-            onClick = { showContent = !showContent },
-            label = {
-                Text(
-                    getCommonString().attachment_sensitive,
-                    color = MaterialTheme.colorScheme.onSecondaryContainer,
-                )
-            },
-            colors = SuggestionChipDefaults.suggestionChipColors(
-                containerColor = MaterialTheme.colorScheme.secondaryContainer,
-            ),
-            border = null,
+        Icon(
+            Icons.Default.VisibilityOff,
+            contentDescription = "Hide Content",
             modifier = Modifier.align(Alignment.Center),
         )
     }
 }
 
 @Composable
-private fun ThumbnailOne(
-    previewUrls: List<Uri>,
-) = AsyncImage(
-    previewUrls[0],
-    contentScale = ContentScale.Crop,
+private fun SensitiveCover(
+    height: Dp,
+    onShowContent: () -> Unit,
+) = Box(
     modifier = Modifier.fillMaxWidth()
-        .height(GridHeight)
-        .clip(RoundedCornerShape(8.dp)),
-)
-
-@Composable
-private fun ThumbnailTwo(
-    previewUrls: List<Uri>,
-) = Row(
-    modifier = Modifier.fillMaxWidth()
-        .height(GridHeight),
+        .height(height)
+        .clip(RoundedCornerShape(8.dp))
+        .background(MaterialTheme.colorScheme.surfaceBright),
 ) {
-    Box(modifier = Modifier.weight(1F)) {
-        AsyncImage(
-            previewUrls[0],
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-                .clip(RoundedCornerShape(8.dp)),
-        )
-    }
-    Spacer(Modifier.width(4.dp))
-    Box(modifier = Modifier.weight(1F)) {
-        AsyncImage(
-            previewUrls[1],
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-                .clip(RoundedCornerShape(8.dp)),
-        )
-    }
-}
-
-@Composable
-private fun ThumbnailThree(
-    previewUrls: List<Uri>,
-) = Row(
-    modifier = Modifier.fillMaxWidth()
-        .height(GridHeight),
-) {
-    Box(modifier = Modifier.weight(1F)) {
-        AsyncImage(
-            previewUrls[0],
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-                .clip(RoundedCornerShape(8.dp)),
-        )
-    }
-    Spacer(Modifier.width(4.dp))
-    Column(
-        modifier = Modifier.weight(1F)
-            .fillMaxHeight(),
-    ) {
-        Box(modifier = Modifier.weight(1F)) {
-            AsyncImage(
-                previewUrls[1],
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-                    .clip(RoundedCornerShape(8.dp)),
+    SuggestionChip(
+        onClick = onShowContent,
+        label = {
+            Text(
+                getCommonString().attachment_sensitive,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
             )
-        }
-        Spacer(Modifier.height(4.dp))
-        Box(modifier = Modifier.weight(1F)) {
-            AsyncImage(
-                previewUrls[2],
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-                    .clip(RoundedCornerShape(8.dp)),
-            )
-        }
-    }
-}
-
-@Composable
-private fun ThumbnailFour(
-    previewUrls: List<Uri>,
-) = Column(
-    modifier = Modifier.fillMaxWidth()
-        .height(GridHeight * 2),
-) {
-    ThumbnailTwo(previewUrls.slice(0..1))
-    Spacer(Modifier.height(4.dp))
-    ThumbnailTwo(previewUrls.slice(2..3))
+        },
+        colors = SuggestionChipDefaults.suggestionChipColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        ),
+        border = null,
+        modifier = Modifier.align(Alignment.Center),
+    )
 }
---
+++ features/shared/src/commonMain/kotlin/noctiluca/features/shared/status/attachment/Thumbnail.kt
@@ -0,0 +1,128 @@
+package noctiluca.features.shared.status.attachment
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import noctiluca.features.shared.components.image.AsyncImage
+import noctiluca.model.Uri
+import noctiluca.model.status.Attachment
+
+internal val GridHeight = 160.dp
+internal const val COUNT_PER_GRID = 4
+
+@Composable
+internal fun ThumbnailGrid(
+    attachments: List<Attachment>,
+) = attachments.filter { it !is Attachment.Audio && it !is Attachment.Unknown }
+    .chunked(COUNT_PER_GRID)
+    .forEach { chunked ->
+        val previewUrls = chunked.map { it.previewUrl }
+
+        when (chunked.size) {
+            1 -> ThumbnailOne(previewUrls)
+            2 -> ThumbnailTwo(previewUrls)
+            3 -> ThumbnailThree(previewUrls)
+            4 -> ThumbnailFour(previewUrls)
+        }
+    }
+
+@Composable
+private fun ThumbnailOne(
+    previewUrls: List<Uri>,
+) = AsyncImage(
+    previewUrls[0],
+    contentScale = ContentScale.Crop,
+    modifier = Modifier.fillMaxWidth()
+        .height(GridHeight)
+        .clip(RoundedCornerShape(8.dp)),
+)
+
+@Composable
+private fun ThumbnailTwo(
+    previewUrls: List<Uri>,
+) = Row(
+    modifier = Modifier.fillMaxWidth()
+        .height(GridHeight),
+) {
+    Box(modifier = Modifier.weight(1F)) {
+        AsyncImage(
+            previewUrls[0],
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+                .clip(RoundedCornerShape(8.dp)),
+        )
+    }
+    Spacer(Modifier.width(4.dp))
+    Box(modifier = Modifier.weight(1F)) {
+        AsyncImage(
+            previewUrls[1],
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+                .clip(RoundedCornerShape(8.dp)),
+        )
+    }
+}
+
+@Composable
+private fun ThumbnailThree(
+    previewUrls: List<Uri>,
+) = Row(
+    modifier = Modifier.fillMaxWidth()
+        .height(GridHeight),
+) {
+    Box(modifier = Modifier.weight(1F)) {
+        AsyncImage(
+            previewUrls[0],
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+                .clip(RoundedCornerShape(8.dp)),
+        )
+    }
+    Spacer(Modifier.width(4.dp))
+    Column(
+        modifier = Modifier.weight(1F)
+            .fillMaxHeight(),
+    ) {
+        Box(modifier = Modifier.weight(1F)) {
+            AsyncImage(
+                previewUrls[1],
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+                    .clip(RoundedCornerShape(8.dp)),
+            )
+        }
+        Spacer(Modifier.height(4.dp))
+        Box(modifier = Modifier.weight(1F)) {
+            AsyncImage(
+                previewUrls[2],
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+                    .clip(RoundedCornerShape(8.dp)),
+            )
+        }
+    }
+}
+
+@Composable
+private fun ThumbnailFour(
+    previewUrls: List<Uri>,
+) = Column(
+    modifier = Modifier.fillMaxWidth()
+        .height(GridHeight * 2),
+) {
+    ThumbnailTwo(previewUrls.slice(0..1))
+    Spacer(Modifier.height(4.dp))
+    ThumbnailTwo(previewUrls.slice(2..3))
+}

452685671929c488deeeb0d1c85a54f12f0a23e0
subroh0508 <subroh.0508@gmail.com>
2024-04-08 00:05:55 +0900

    WIP

--- features/page/attachment/src/iosMain/kotlin/noctiluca/features/attachment/component/IosVideoPlayer.kt
+++ features/page/attachment/src/iosMain/kotlin/noctiluca/features/attachment/component/IosVideoPlayer.kt
@@ -3,18 +3,18 @@ package noctiluca.features.attachment.component
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.interop.LocalUIViewController
 import androidx.compose.ui.interop.UIKitView
+import cafe.adriel.voyager.navigator.LocalNavigator
 import kotlinx.cinterop.CValue
 import kotlinx.cinterop.ExperimentalForeignApi
 import noctiluca.model.Uri
 import platform.AVFoundation.AVPlayer
-import platform.AVFoundation.AVPlayerLayer
 import platform.AVFoundation.play
 import platform.AVKit.AVPlayerViewController
 import platform.CoreGraphics.CGRect
 import platform.Foundation.NSURL
 import platform.QuartzCore.CATransaction
-import platform.QuartzCore.kCAGravityResizeAspectFill
 import platform.QuartzCore.kCATransactionDisableActions
 import platform.UIKit.UIView
 
@@ -29,31 +29,38 @@ actual fun VideoPlayer(
     val nsUrl = NSURL.URLWithString(url.value) ?: return
 
     val player = remember { AVPlayer(uRL = nsUrl) }
-    val playerLayer = remember { AVPlayerLayer() }
-    val avPlayerViewController = remember { AVPlayerViewController() }
+    val playerViewController = remember { AVPlayerViewController() }
+    val localViewController = LocalUIViewController.current
+    val navigator = LocalNavigator.current
 
-    avPlayerViewController.player = player
-    avPlayerViewController.showsPlaybackControls = true
-
-    playerLayer.player = player
+    playerViewController.player = player
+    playerViewController.showsPlaybackControls = true
+    playerViewController.entersFullScreenWhenPlaybackBegins = true
+    playerViewController.exitsFullScreenWhenPlaybackEnds = true
 
     UIKitView(
         factory = {
             UIView().apply {
-                addSubview(avPlayerViewController.view)
+                localViewController.presentViewController(
+                    playerViewController,
+                    animated = true,
+                    completion = null,
+                )
             }
+        },
+        update = {
+            player.play()
+            playerViewController.player?.play()
+        },
+        onRelease = {
+            println("onRelease")
         },
         onResize = { view: UIView, rect: CValue<CGRect> ->
             CATransaction.begin()
             CATransaction.setValue(true, kCATransactionDisableActions)
             view.layer.setFrame(rect)
-            playerLayer.setFrame(rect)
-            avPlayerViewController.view.layer.frame = rect
+            playerViewController.view.layer.frame = rect
             CATransaction.commit()
-        },
-        update = {
-            player.play()
-            avPlayerViewController.player?.play()
         },
         modifier = modifier,
     )

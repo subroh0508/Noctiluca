c1a346bbb5bd5d8c413713039f6c1b058b1a2149
subroh0508 <subroh.0508@gmail.com>
2024-04-29 22:58:10 +0900

    Add InstanceHeaderTextTest.kt

--- features/page/signin/src/commonTest/kotlin/noctiluca/features/sigin/spec/component/ActionButtonsTest.kt
+++ features/page/signin/src/commonTest/kotlin/noctiluca/features/sigin/spec/component/ActionButtonsTest.kt
@@ -9,43 +9,15 @@ import androidx.compose.ui.test.runComposeUiTest
 import androidx.compose.ui.unit.dp
 import io.kotest.matchers.be
 import io.kotest.matchers.should
-import noctiluca.features.sigin.HTML_INSTANCE_DESCRIPTION
-import noctiluca.features.sigin.INSTANCE_DESCRIPTION
-import noctiluca.features.sigin.INSTANCE_DOMAIN
-import noctiluca.features.sigin.INSTANCE_NAME
 import noctiluca.features.sigin.TestComposable
+import noctiluca.features.sigin.mock.instance
 import noctiluca.features.signin.SignInTestTag
 import noctiluca.features.signin.component.ActionButtons
-import noctiluca.model.Uri
 import noctiluca.model.authorization.Instance
 import kotlin.test.Test
 
 @OptIn(ExperimentalTestApi::class)
 class ActionButtonsTest {
-    private val instance = Instance(
-        name = INSTANCE_NAME,
-        domain = INSTANCE_DOMAIN,
-        description = INSTANCE_DESCRIPTION,
-        thumbnail = Uri("https://media.mstdn.social/site_uploads/thumbnail.png"),
-        languages = listOf("ja"),
-        activeUserCount = 10_000,
-        administrator = Instance.Administrator(
-            screen = "@stux@mstdn.social",
-            displayName = "stux⚡",
-            url = Uri("https://mstdn.social/@stux"),
-            avatar = Uri("https://media.mstdn.social/accounts/avatars/avatar.png"),
-        ),
-        rules = listOf(
-            Instance.Rule(
-                "aaa",
-                "Sexually explicit or violent media must be marked as sensitive when posting."
-            ),
-            Instance.Rule("bbb", "No spam or advertising."),
-        ),
-        extendedDescription = HTML_INSTANCE_DESCRIPTION,
-        version = Instance.Version(4, 2, 8),
-    )
-
     @Test
     fun AuthorizeButton_shouldNotDisplay() = runComposeUiTest {
         setContent {
---
+++ features/page/signin/src/commonTest/kotlin/noctiluca/features/sigin/spec/component/InstanceHeaderTextTest.kt
@@ -0,0 +1,88 @@
+package noctiluca.features.sigin.spec.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performScrollToNode
+import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.unit.dp
+import noctiluca.features.sigin.INSTANCE_DOMAIN
+import noctiluca.features.sigin.TestComposable
+import noctiluca.features.sigin.mock.instance
+import noctiluca.features.signin.component.InstancesTab
+import noctiluca.features.signin.component.topappbar.InstanceHeaderText
+import noctiluca.features.signin.section.scrollableframe.rememberInstanceDetailScrollableFrameState
+import kotlin.test.Test
+
+private const val DUMMY_LAZY_COLUMN = "DummyLazyColumn"
+
+@OptIn(ExperimentalTestApi::class)
+class InstanceHeaderTextTest {
+    @Test
+    fun InstanceHeaderText_shouldShowDomainText() = runComposeUiTest {
+        setContent {
+            val scrollState = rememberInstanceDetailScrollableFrameState(
+                InstancesTab.Info,
+                listOf(),
+            )
+
+            TestComposable {
+                InstanceHeaderText(
+                    domain = INSTANCE_DOMAIN,
+                    instance = null,
+                    scrollState = scrollState,
+                )
+            }
+        }
+
+        onNodeWithText(INSTANCE_DOMAIN).assertExists()
+        onNodeWithText(instance.name).assertDoesNotExist()
+    }
+
+    @Test
+    fun InstanceHeaderText_shouldShowDomainAndNameText() = runComposeUiTest {
+        setContent {
+            val scrollState = rememberInstanceDetailScrollableFrameState(
+                InstancesTab.Info,
+                listOf(),
+            )
+
+            TestComposable {
+                Box(Modifier.height(100.dp)) {
+                    LazyColumn(
+                        state = scrollState.lazyListState,
+                        modifier = Modifier.fillMaxSize()
+                            .testTag(DUMMY_LAZY_COLUMN),
+                    ) {
+                        items(List(5) { "Item #$it" }) {
+                            Text(it, Modifier.height(80.dp))
+                        }
+                    }
+
+                    InstanceHeaderText(
+                        domain = instance.domain,
+                        instance = instance,
+                        scrollState = scrollState,
+                    )
+                }
+            }
+        }
+
+        onNodeWithText(instance.domain).assertDoesNotExist()
+        onNodeWithText(instance.name).assertDoesNotExist()
+
+        onNodeWithTag(DUMMY_LAZY_COLUMN).performScrollToNode(hasText("Item #4"))
+
+        onNodeWithText(instance.domain).assertExists()
+        onNodeWithText(instance.name).assertExists()
+    }
+}

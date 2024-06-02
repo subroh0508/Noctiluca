dafe72cb6d81c90f050e828a72e0e500f77f755e
subroh0508 <subroh.0508@gmail.com>
2024-06-02 17:04:33 +0900

    Add MediaRepository to Module

--- core/data/src/commonMain/kotlin/noctiluca/data/di/DataStatusModule.kt
+++ core/data/src/commonMain/kotlin/noctiluca/data/di/DataStatusModule.kt
@@ -1,7 +1,9 @@
 package noctiluca.data.di
 
+import noctiluca.data.status.MediaRepository
 import noctiluca.data.status.StatusIndexRepository
 import noctiluca.data.status.StatusRepository
+import noctiluca.data.status.impl.MediaRepositoryImpl
 import noctiluca.data.status.impl.StatusIndexRepositoryImpl
 import noctiluca.data.status.impl.StatusRepositoryImpl
 import org.koin.core.module.Module
@@ -9,12 +11,14 @@ import org.koin.dsl.ScopeDSL
 
 @Suppress("FunctionName")
 fun ScopeDSL.DataStatusModule() {
+    scoped<MediaRepository> { MediaRepositoryImpl(get()) }
     scoped<StatusRepository> { StatusRepositoryImpl(get(), get()) }
     scoped<StatusIndexRepository> { StatusIndexRepositoryImpl(get(), get()) }
 }
 
 @Suppress("FunctionName")
 fun Module.TestDataStatusModule() {
+    single<MediaRepository> { MediaRepositoryImpl(get()) }
     single<StatusRepository> { StatusRepositoryImpl(get(), get()) }
     single<StatusIndexRepository> { StatusIndexRepositoryImpl(get(), get()) }
 }

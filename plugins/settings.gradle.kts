dbd0f7ac5e939efe780c22bef7480249a47a056d
subroh0508 <subroh.0508@gmail.com>
2024-06-24 23:30:31 +0900

    Add dependency

--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -1,6 +1,9 @@
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+        google()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     versionCatalogs {
